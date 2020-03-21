2eb0836e7f8cea5403a6f1c053326ffaf74a42ec
Tanmay Sinha <alokratn001@gmail.com>
2020-03-22 04:28:02 +0530

    linked contractor with server

--- database/database.py
+++ database/database.py
@@ -9,6 +9,19 @@ cursor = conn.cursor()
 cursor.execute("SELECT max(labourer_id) from labourer_details")
 count_labourers = cursor.fetchone()[0]
 
+skill_2 = {1: 7,
+         2: 6,
+         3: 9,
+         4: 7,
+         5: 6,
+         6: 5,
+         7: 10,
+         8: 8,
+         9: 8,
+         10: 6
+         }
+
+
 def create_table():
     cursor.execute('''CREATE TABLE IF NOT EXISTS labourer_details(
         labourer_id INTEGER,
@@ -41,23 +54,23 @@ def find_distance(contractor_lat, contractor_long,
                   labour_lat, labour_long):
     coord1 = (contractor_lat, contractor_long)
     coord2 = (labour_lat, labour_long)
-    return dis(coord1, coord2).km
+    return round(dis(coord1, coord2).km, 3)
 
 
 def data_retrieval(skill, latitudes, longitudes):
     cursor.execute(
         f'''SELECT * FROM labourer_details WHERE skill={skill}'''
     )
-    input_data = []
+    input_data = {}
     for row in cursor.fetchall():
-        labourer = [row[2], row[4], row[7]]
-        distance = find_distance(latitudes, longitudes, row[5], row[6])
-        distance = round(distance, 3)
-        labourer.append(distance)
-        input_data.append(labourer)
-    # print(input_data)
+        id_r = row[0]
+        input_data[id_r] = [row[0], row[4], row[7]]
+        input_data[id_r].append(find_distance(
+            latitudes, longitudes, row[5], row[6]
+        ))
+        input_data[id_r].append(skill_2[skill])
+    print(input_data)
     return input_data
-
 
 
 if __name__ == "__main__":
@@ -66,18 +79,19 @@ if __name__ == "__main__":
     # names = ['tanmay', 'anshuman', 'ankan', 'shreyash',
     #          'raju', 'babu rao', 'shyam', 'hello', 'world']
     # for i in range(51):
-        # labourer_id = i+1
-    # name = random.choice(names)
-    # age = random.randint(18, 60)
-    # skill = random.randint(1, 10)
-    # gender = random.randint(0, 1)
-    # latitudes = round(random.uniform(23.65, 23.85), 3)
-    # longitudes = round(random.uniform(86.30, 86.50), 3)
-    # income = random.randint(1, 10)*100
+    #    labourer_id = i+1
+    #    name = random.choice(names)
+    #    age = random.randint(18, 60)
+    #    skill = random.randint(1, 10)
+    #    gender = random.randint(0, 1)
+    #    latitudes = round(random.uniform(23.65, 23.85), 3)
+    #    longitudes = round(random.uniform(86.30, 86.50), 3)
+    #    income = random.randint(1, 10)*100
 
     # data_entry(name, age, skill, gender,
     #           latitudes, longitudes, income)
     #     time.sleep(1)
+    # print(skill[3])
     # data_retrieval(5, 23.43, 86.5)
     # cursor.close()
     # conn.close()
--- main.py
+++ main.py
@@ -2,22 +2,46 @@ from flask import Flask
 from flask import request
 import json
 import database.database as db
+import model.model as predictor
 app = Flask(__name__)
+
+obj = predictor.Model()
+
+skills = {"Carpentary": [1, 7, "-"],
+          "Painting": [2, 6, "+"],
+          "Masonry": [3, 9, "-"],
+          "Plumbing": [4, 7, "+"],
+          "Electrician": [5, 6, "+"],
+          "Cleaner": [6, 5, "+"],
+          "Rigger": [7, 10, "-"],
+          "Transport": [8, 5, "+"],
+          "Welder": [9, 8, "-"],
+          "Fitter": [10, 6, "-"]
+          }
+
 
 @app.route("/add_database", methods=['POST'])
 def add_labour_database():
     if request.method == 'POST':
         data = request.json
-        print(data["name"])
-        db.data_entry(data["name"], data["age"],2 ,data["gender"],
-        23.4, 86.5, 200)
+
+        db.data_entry(data["name"], data["age"], skills[data["skills"]][0], data["gender"],
+                      round(data["location"]["latitude"],3), round(data["location"]["longitude"], 3), 0)
         return 'OK'
+
 
 @app.route("/get_details", methods=['GET', 'POST'])
 def get_details_from_database():
     if request.method == 'POST':
         data = request.json
+        skill = skills[data["skill"]]
+        input_data = db.data_retrieval(skill[0], round(data["location"]["latitude"], 3),
+                          round(data["location"]["longitude"], 3))
+        
+        pre, index = obj.preprocess_input(input_data, skill[2])
+        ans = obj.predict(pre, skill[2], index)
+        return 'OK'
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
