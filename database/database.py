e469941e104ac17de191cd0f841df36701e9870d
Tanmay Sinha <alokratn001@gmail.com>
2020-03-22 08:14:01 +0530

    added repsonse to contractor

--- database/database.py
+++ database/database.py
@@ -1,3 +1,5 @@
+''' Entering, retrieving and maintaining the labourer's database. '''
+
 import sqlite3
 import random
 import time
@@ -7,22 +9,24 @@ conn = sqlite3.connect('database/labourers.db', check_same_thread=False)
 cursor = conn.cursor()
 
 cursor.execute("SELECT max(labourer_id) from labourer_details")
+global count_labourers
 count_labourers = cursor.fetchone()[0]
 
 skill_2 = {1: 7,
-         2: 6,
-         3: 9,
-         4: 7,
-         5: 6,
-         6: 5,
-         7: 10,
-         8: 8,
-         9: 8,
-         10: 6
-         }
+           2: 6,
+           3: 9,
+           4: 7,
+           5: 6,
+           6: 5,
+           7: 10,
+           8: 8,
+           9: 8,
+           10: 6
+           }
 
 
 def create_table():
+    ''' creating the table in the database '''
     cursor.execute('''CREATE TABLE IF NOT EXISTS labourer_details(
         labourer_id INTEGER,
         name TEXT,
@@ -31,20 +35,24 @@ def create_table():
         gender INTEGER,
         latitudes REAL,
         longitudes REAL,
-        income INTEGER
+        income INTEGER,
+        contact TEXT
     )''')
 
 
 def data_entry(name, age, skill, gender,
-               latitudes, longitudes, income):
+               latitudes, longitudes, income, contact):
+    '''Entering data into the database'''
+    global count_labourers
     labourer_id = count_labourers + 1
+
     cursor.execute('''INSERT INTO labourer_details (
         labourer_id, name, age, skill, gender,
-               latitudes, longitudes, income
-    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+               latitudes, longitudes, income, contact
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
     ''', (
         labourer_id, name, age, skill, gender,
-        latitudes, longitudes, income
+        latitudes, longitudes, income, contact
     ))
     count_labourers = count_labourers + 1
     conn.commit()
@@ -52,12 +60,14 @@ def data_entry(name, age, skill, gender,
 
 def find_distance(contractor_lat, contractor_long,
                   labour_lat, labour_long):
+    '''finding distance between two co-ordinates on map'''
     coord1 = (contractor_lat, contractor_long)
     coord2 = (labour_lat, labour_long)
     return round(dis(coord1, coord2).km, 3)
 
 
 def data_retrieval(skill, latitudes, longitudes):
+    '''retreiving the data from database to be sent to the model'''
     cursor.execute(
         f'''SELECT * FROM labourer_details WHERE skill={skill}'''
     )
@@ -69,34 +79,56 @@ def data_retrieval(skill, latitudes, longitudes):
             latitudes, longitudes, row[5], row[6]
         ))
         input_data[id_r].append(skill_2[skill])
-    print(input_data)
+    # /print(input_data)
     return input_data
 
 
+def get_complete_details(id_, dist):
+    '''getting details to be sent to the contractor'''
+    cursor.execute(
+        f'''SELECT * FROM labourer_details WHERE labourer_id={id_}'''
+    )
+    jsondata = {}
+    for row in cursor.fetchall():
+        jsondata["name"] = row[1]
+        jsondata["age"] = row[2]
+        if row[4] == 1:
+            jsondata["gender"] = "male"
+        else:
+            jsondata["gender"] = "female"
+        jsondata["distance"] = dist
+        jsondata["contact"] = row[8]
+
+    return jsondata
+
+
+def close_databae():
+    '''closing the database'''
+    cursor.close()
+    conn.close()
+
+
 if __name__ == "__main__":
-    pass
-    # create_table()
-    # names = ['tanmay', 'anshuman', 'ankan', 'shreyash',
-    #          'raju', 'babu rao', 'shyam', 'hello', 'world']
-    # for i in range(51):
-    #    labourer_id = i+1
-    #    name = random.choice(names)
-    #    age = random.randint(18, 60)
-    #    skill = random.randint(1, 10)
-    #    gender = random.randint(0, 1)
-    #    latitudes = round(random.uniform(23.65, 23.85), 3)
-    #    longitudes = round(random.uniform(86.30, 86.50), 3)
-    #    income = random.randint(1, 10)*100
 
-    # data_entry(name, age, skill, gender,
-    #           latitudes, longitudes, income)
-    #     time.sleep(1)
-    # print(skill[3])
-    # data_retrieval(5, 23.43, 86.5)
-    # cursor.close()
-    # conn.close()
+    ''' Generating random datasets for testing'''
+    create_table()
+    names = ['tanmay', 'anshuman', 'ankan', 'shreyash',
+             'raju', 'babu rao', 'shyam', 'hello', 'world']
+    for i in range(51):
+        labourer_id = i+1
+        name = random.choice(names)
+        age = random.randint(18, 60)
+        skill = random.randint(1, 10)
+        gender = random.randint(0, 1)
+        latitudes = round(random.uniform(23.65, 23.85), 3)
+        longitudes = round(random.uniform(86.30, 86.50), 3)
+        income = random.randint(1, 10)*100
+        contact = ''.join(["{}".format(random.randint(0, 9))
+                           for num in range(0, 10)])
+        data_entry(name, age, skill, gender,
+                   latitudes, longitudes, income, contact)
 
-    # cor1 = (23.745541, 86.343521)
-    # cor2 = (23.412358, 85.439989)
-
-    # print(distance(cor1, cor2))
+        # time.sleep(1)
+    data_retrieval(5, 23.43, 86.5)
+    cursor.close()
+    conn.close()
